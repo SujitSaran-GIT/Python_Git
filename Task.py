6f683ad4f6173f5c9dd0caf78e53f4c060082e13
SujitSaran-GIT <sujitsaran82@gmail.com>
2024-11-11 13:09:41 +0530

    String

--- Task.py
+++ Task.py
@@ -1,2 +1,10 @@
+# Introduction
+# The string is the combination of charecters, and To tell our system it is a string we use this inside the double quotation
 print("Hello world");
 print("Hello world")
+# String concatenation
+# The string is the combination of charecters, and To tell our system it is a string we use this inside the double quotation
+print("My name is sujit saran \n I am from Jajpur")
+print("Hello "+" My name is sujit saran")
+# Indentation Error
+#     print("Hello") Throws an Error
